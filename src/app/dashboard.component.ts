d29116f407481f7c5d25dbcb78898086b5a8c96f
hirose <superblueplanet@gmail.com>
2017-06-24 17:08:13 +0900

    routing to dashboard.

---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {HeroService} from './hero.service';
+import {Hero} from './hero';
+
+@Component({
+  selector: 'app-dashboard',
+  template: `
+    <h3>My Dashboard</h3>
+  `,
+  templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[];
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5 ));
+  }
+
+  constructor(private heroService: HeroService) {}
+}
